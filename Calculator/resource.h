7158e2a1c77edfa88b336449e44e13438c5b5cfc
현태 김 <lemontow@naver.com>
2022-04-30 13:00:33 +0900

    계산기

---
+++ Calculator/resource.h
@@ -0,0 +1,47 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// Calculator.rc에서 사용되고 있습니다.
+//
+#define IDC_MYICON                      2
+#define IDD_CALCULATOR_DIALOG           102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_CALCULATOR                  107
+#define IDI_SMALL                       108
+#define IDC_CALCULATOR                  109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define NUM1                            1000
+#define NUM2                            1001
+#define NUM3                            1002
+#define NUM4                            1003
+#define NUM5                            1004
+#define NUM6                            1005
+#define NUM7                            1006
+#define NUM8                            1007
+#define NUM9                            1008
+#define IDC_MINUS                       1009
+#define NUM0                            1010
+#define IDC_PLUS                        1011
+#define NUM13                           1012
+#define CAL                             1012
+#define IDC_CAL                         1012
+#define IDC_MULTI                       1013
+#define NUM15                           1014
+#define IDC_DIV                         1014
+#define IDC_SHOW                        1015
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1016
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
